a2dbf906c39ed2e5e9a4b61931c430611d2b2178
seanmcquaid <sean.mcquaid2@verizon.net>
2022-06-16 08:26:06 -0500

    removed css files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-import './index.css';
 import './i18n';
 import theme from './theme';
 import GlobalStyle from './theme/GlobalStyle';
