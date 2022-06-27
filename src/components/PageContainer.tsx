124b3cad6806b5e2a9f4179ec6e92171c150f1fc
seanmcquaid <sean.mcquaid2@verizon.net>
2022-06-26 21:54:35 -0400

    text input and page container

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
-const PageContainer = styled.div``;
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+`;
 
 export default PageContainer;
