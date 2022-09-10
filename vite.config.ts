1ab7b890a1f630e7aa68fcb2d20d87f21caad62e
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-09-09 21:57:06 -0400

    pwa set up

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,34 @@ export default defineConfig(async () => {
       legacy(),
       checker({ typescript: true }),
       splitVendorChunkPlugin(),
-      VitePWA({ registerType: 'autoUpdate' }),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['vite.svg', 'favicon.ico', 'apple-touch-icon.png'],
+        manifest: {
+          name: 'My Awesome App',
+          short_name: 'MyApp',
+          description: 'My Awesome App description',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
     ],
     test: {
       globals: true,
