94117f4ce37a36577624c187a6e29cd5515d8209
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-09-11 17:47:48 -0400

    added zod validation

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,8 +1,12 @@
-interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
+import { z } from 'zod';
+
+export const PostSchema = z.object({
+  userId: z.number(),
+  id: z.number(),
+  title: z.string(),
+  body: z.string(),
+});
+
+type Post = z.infer<typeof PostSchema>;
 
 export default Post;
