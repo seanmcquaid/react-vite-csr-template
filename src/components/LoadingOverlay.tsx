484e6db0aea3dff27ee2c27dda5a5bf59540432c
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-20 19:54:29 +0200

    loading overlay

--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -6,7 +6,13 @@ interface LoadingOverlayProps {
 }
 
 const LoadingOverlay: FC<LoadingOverlayProps> = ({ isLoading }) => (
-  <div className="fixed hidden inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
+  <div
+    className={
+      isLoading
+        ? 'fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center'
+        : 'hidden'
+    }
+  >
     <ClipLoader loading={isLoading} />
   </div>
 );
