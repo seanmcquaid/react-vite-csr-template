71e26dc17b92537b785c288e6a2b113f61e94142
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-22 00:19:07 +0200

    added deferring

--- src/pages/PostDetails/postDetailsLoader.ts
+++ src/pages/PostDetails/postDetailsLoader.ts
@@ -1,19 +1,23 @@
-import { LoaderFunctionArgs } from 'react-router-dom';
+import { defer, LoaderFunctionArgs } from 'react-router-dom';
 import store from '../../store';
 import postsApi from '../../store/postsApi';
 import Post from '../../types/Post';
 
-const postDetailsLoader = ({
-  params,
-}: LoaderFunctionArgs): Post | Promise<Post> => {
+export interface PostDetailsLoaderData {
+  postInfo: Post | Promise<Post>;
+}
+
+const postDetailsLoader = ({ params }: LoaderFunctionArgs) => {
   const { id } = params;
   if (!id) {
     throw new Error('An ID is required');
   }
   const { data } = postsApi.endpoints.getPostById.select(id)(store.getState());
-  return (
-    data ?? store.dispatch(postsApi.endpoints.getPostById.initiate(id)).unwrap()
-  );
+  return defer({
+    postInfo:
+      data ??
+      store.dispatch(postsApi.endpoints.getPostById.initiate(id)).unwrap(),
+  });
 };
 
 export default postDetailsLoader;
