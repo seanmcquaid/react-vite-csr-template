790ba542a0021e5fe2866faa68734212d39598ad
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-21 18:04:14 +0200

    removed use deferred value

--- src/pages/PostDetails/PostInfo.tsx
+++ src/pages/PostDetails/PostInfo.tsx
@@ -1,4 +1,4 @@
-import { FC, useDeferredValue } from 'react';
+import { FC } from 'react';
 import Post from '../../types/Post';
 
 interface PostInfoProps {
@@ -6,12 +6,7 @@ interface PostInfoProps {
 }
 
 const PostInfo: FC<PostInfoProps> = ({ post }) => {
-  // This hook is commonly used to keep the interface responsive when you have something that renders immediately based on user input and something that needs to wait for a data fetch.
-  // A good example of this is a text input that triggers an additional change on the page due to a state change in a 3rd party library where we can't control state.
-  // For example, using RTK query to handle fetching the data but we can't explictly control if/when the data is done loading
-  const postInfo = useDeferredValue(post);
-
-  if (!postInfo) {
+  if (!post) {
     return null;
   }
 
