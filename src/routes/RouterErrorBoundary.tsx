19ac91f807858c0eeeca512a1ea6bbed77375292
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-22 00:05:32 +0200

    fixed linter

--- src/routes/RouterErrorBoundary.tsx
+++ src/routes/RouterErrorBoundary.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react';
-import { useRouteError } from 'react-router-dom';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 
 const RouterErrorBoundary: FC = () => {
   const error = useRouteError();
 
-  return <div />;
+  if (isRouteErrorResponse(error)) {
+    // eslint-disable-next-line i18next/no-literal-string
+    return <div>Router error response</div>;
+  }
+
+  // eslint-disable-next-line i18next/no-literal-string
+  return <div>Something went wrong</div>;
 };
 
 export default RouterErrorBoundary;
