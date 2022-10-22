736febec2c8e73b3682b2f32f920eeea229f8d95
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-22 18:58:21 +0200

    added instanbul

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import loadVersion from 'vite-plugin-package-version';
 import svgr from 'vite-plugin-svgr';
 import legacy from '@vitejs/plugin-legacy';
 import checker from 'vite-plugin-checker';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -18,13 +19,23 @@ export default defineConfig(() => {
       legacy(),
       checker({ typescript: true }),
       splitVendorChunkPlugin(),
+      istanbul({
+        include: 'src/*',
+        exclude: ['node_modules', 'test/'],
+        extension: ['.js', '.ts', '.jsx', '.tsx'],
+        requireEnv: true,
+      }),
     ],
     test: {
       globals: true,
       environment: 'jsdom',
       setupFiles: ['./src/setupTests.ts'],
       coverage: {
+        provider: 'istanbul',
         reporter: ['lcov'],
+        all: true,
+        include: ['src/**/*.ts', 'src/**/*.tsx'],
+        exclude: ['src/setupTests.ts', 'src/testUtils'],
       },
     },
   };
