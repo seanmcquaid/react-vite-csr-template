bae91fe9d195bb4f52164affd0345f2d6ac13034
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-22 18:59:04 +0200

    slimming config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import loadVersion from 'vite-plugin-package-version';
 import svgr from 'vite-plugin-svgr';
 import legacy from '@vitejs/plugin-legacy';
 import checker from 'vite-plugin-checker';
-import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -19,12 +18,6 @@ export default defineConfig(() => {
       legacy(),
       checker({ typescript: true }),
       splitVendorChunkPlugin(),
-      istanbul({
-        include: 'src/*',
-        exclude: ['node_modules', 'test/'],
-        extension: ['.js', '.ts', '.jsx', '.tsx'],
-        requireEnv: true,
-      }),
     ],
     test: {
       globals: true,
