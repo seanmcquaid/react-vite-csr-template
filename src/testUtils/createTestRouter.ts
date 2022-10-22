4fa646caa44db760e46e2e1f189bdf7cd380b114
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-22 18:15:43 +0200

    remove unused file

--- src/testUtils/createTestRouter.ts
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom';
-
-interface CreateTestRouterArgs {
-  initialRoute: string;
-}
-
-const createTestRouter = () => createBrowserRouter([]);
-
-export default createTestRouter;
