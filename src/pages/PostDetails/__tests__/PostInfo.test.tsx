330adce864da412ae6bb7f394637a9d418f859c5
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-23 16:40:29 +0200

    added a test for post details loader

--- src/pages/PostDetails/__tests__/PostInfo.test.tsx
+++ src/pages/PostDetails/__tests__/PostInfo.test.tsx
@@ -1,45 +1,9 @@
-import { rest } from 'msw';
-import { screen, waitFor } from '@testing-library/react';
-import { Route } from 'react-router-dom';
-import { render } from '../../../testUtils/reactTestingLibraryUtils';
-import PostDetails from '../PostDetails';
-import mswServer from '../../../testUtils/mswServer';
-import RouteConstants from '../../../routes/RouteConstants';
-import postDetailsLoader from '../postDetailsLoader';
+import { render, screen } from '../../../testUtils/reactTestingLibraryUtils';
+import PostInfo from '../PostInfo';
 
-describe('PostDetails', () => {
-  it('does not display post info if no data comes back from the API', async () => {
-    mswServer.use(
-      rest.get(
-        'https://jsonplaceholder.typicode.com/posts/1',
-        (req, res, context) => {
-          return res(context.status(500));
-        },
-      ),
-    );
-    render(
-      <Route
-        path={RouteConstants.POST_DETAILS}
-        element={<PostDetails />}
-        loader={postDetailsLoader}
-      />,
-      { initialRoute: '/post/1' },
-    );
-    await waitFor(() =>
-      expect(screen.queryByTestId('post-info')).not.toBeInTheDocument(),
-    );
-  });
-  it('displays post info if it comes back from the API', async () => {
-    render(
-      <Route
-        path={RouteConstants.POST_DETAILS}
-        element={<PostDetails />}
-        loader={postDetailsLoader}
-      />,
-      { initialRoute: '/post/1' },
-    );
-    await waitFor(() =>
-      expect(screen.queryByTestId('post-info')).toBeInTheDocument(),
-    );
+describe('PostInfo', () => {
+  it('Does not display a post if no post is passed in', () => {
+    render(<PostInfo post={null} />);
+    expect(screen.queryByTestId('post-info')).toBeNull();
   });
 });
