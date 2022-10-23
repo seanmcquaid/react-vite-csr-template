14bb0567cec9728af2b798e09ece7aad7f31106e
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-23 16:30:00 +0200

    improved coverage for not found

---
+++ src/pages/NotFound/__tests__/NotFound.test.tsx
@@ -0,0 +1,30 @@
+import { Route } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import {
+  render,
+  screen,
+  act,
+  waitFor,
+} from '../../../testUtils/reactTestingLibraryUtils';
+import NotFound from '../NotFound';
+import TranslationConstants from '../../../i18n/TranslationConstants';
+
+describe('NotFound', () => {
+  it('Navigates the user back to home when the button is clicked', async () => {
+    render(
+      <>
+        <Route element={<div data-testid="home" />} path="/" />
+        <Route element={<NotFound />} path="*" />
+      </>,
+      { initialRoute: '/notFound' },
+    );
+    await act(async () => {
+      await userEvent.click(
+        screen.getByText(TranslationConstants.NotFound.home),
+      );
+    });
+    await waitFor(() =>
+      expect(screen.queryByTestId('home')).toBeInTheDocument(),
+    );
+  });
+});
