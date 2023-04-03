021ba9bfa3f69a0380492e308350e24929cac25d
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-04-03 18:51:46 -0400

    remove polyfill

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom';
 import 'cross-fetch/polyfill';
-import 'formdata-polyfill';
 import matchers from '@testing-library/jest-dom/matchers';
 import server from './mocks/server';
 
