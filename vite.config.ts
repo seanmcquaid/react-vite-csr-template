4724fdcade6f00958ad7ce0a332625fb6617cf80
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-04-16 18:25:08 -0400

    vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig(() => {
       globals: true,
       environment: 'jsdom',
       setupFiles: ['./src/setupTests.ts'],
+      exclude: ['e2e', 'node_modules'],
       coverage: {
         provider: 'istanbul',
         reporter: ['lcov'],
