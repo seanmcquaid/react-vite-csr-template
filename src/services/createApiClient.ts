dae5bbaed9907198a9b9b30a903aa924cb88c4ff
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-04-22 01:14:34 -0400

    wip

--- src/services/createApiClient.ts
+++ src/services/createApiClient.ts
@@ -3,6 +3,24 @@ import ky from 'ky';
 const createApiClient = (baseUrl: string) => {
   return ky.create({
     prefixUrl: baseUrl,
+    hooks: {
+      beforeRequest: [],
+      beforeRetry: [],
+      afterResponse: [
+        async (_, options, response) => {
+          if (options.validationSchema) {
+            const data = await response.json();
+            options.validationSchema.parse(data);
+          }
+          return response;
+        },
+      ],
+      beforeError: [
+        async (request, options, error) => {
+          console.log(error);
+        },
+      ],
+    },
   });
 };
 
