839f31fab783c60fe53d5697da1fef2d2dad0d3a
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-04-22 13:49:09 -0400

    Small improvements

--- src/services/createApiClient.ts
+++ src/services/createApiClient.ts
@@ -3,6 +3,11 @@ import ky from 'ky';
 const createApiClient = (baseUrl: string) => {
   return ky.create({
     prefixUrl: baseUrl,
+    retry: {
+      limit: 2,
+      methods: ['get', 'put', 'head', 'delete', 'options', 'trace'],
+      statusCodes: [408, 413, 429, 500, 502, 503, 504, 401, 403],
+    },
     hooks: {
       beforeRequest: [
         request => {
@@ -11,24 +16,24 @@ const createApiClient = (baseUrl: string) => {
       ],
       beforeRetry: [
         async ({ request }) => {
-          const token = await ky('https://example.com/refresh-token');
+          const token = await ky.get('https://example.com/refresh-token');
           request.headers.set('Authorization', `token ${token}`);
         },
       ],
       afterResponse: [
         async (_, options, response) => {
-          if (options.validationSchema) {
-            const data = await response.json();
-            return new Response(
-              JSON.stringify(options.validationSchema.parse(data)),
-            );
+          if (!options.validationSchema) {
+            return response;
           }
-          return response;
+
+          const data = await response.json();
+          return new Response(
+            JSON.stringify(options.validationSchema.parse(data)),
+          );
         },
       ],
       beforeError: [
         error => {
-          console.error(error);
           return error;
         },
       ],
