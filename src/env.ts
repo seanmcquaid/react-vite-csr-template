77f2f82d028819c197895a7987d4e6977fc50d44
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-04-22 16:56:59 -0400

    cleaner env

--- src/env.ts
+++ src/env.ts
@@ -1,19 +1,9 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  VITE_APP_ENVIRONMENT: z.union([
-    z.literal('dev'),
-    z.literal('qa'),
-    z.literal('staging'),
-    z.literal('prod'),
-    z.literal('cypress'),
-  ]),
+  VITE_APP_ENVIRONMENT: z.enum(['dev', 'qa', 'staging', 'prod', 'cypress']),
   VITE_APP_MSW_ENABLED: z.coerce.boolean().optional(),
-  MODE: z.union([
-    z.literal('development'),
-    z.literal('test'),
-    z.literal('production'),
-  ]),
+  MODE: z.enum(['development', 'test', 'production']),
   VITE_APP_VERSION: z.string(),
 });
 
