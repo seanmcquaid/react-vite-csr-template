e2c615116ba46c28c289959a28aa4fa6a2527222
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-04-23 13:44:27 -0400

    remove playwright as app environment

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  VITE_APP_ENVIRONMENT: z.enum(['dev', 'qa', 'staging', 'prod', 'playwright']),
+  VITE_APP_ENVIRONMENT: z.enum(['dev', 'qa', 'staging', 'prod']),
   VITE_APP_MSW_ENABLED: z.coerce.boolean().optional(),
   MODE: z.enum(['development', 'test', 'production']),
   VITE_APP_VERSION: z.string(),
