368c3bd3774d68f2871c397078fc50e3a913ac4a
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-04-23 13:36:34 -0400

    naming update

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,14 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  VITE_APP_ENVIRONMENT: z.enum(['dev', 'qa', 'staging', 'prod', 'cypress']),
+  VITE_APP_ENVIRONMENT: z.enum(['dev', 'qa', 'staging', 'prod', 'playwright']),
   VITE_APP_MSW_ENABLED: z.coerce.boolean().optional(),
   MODE: z.enum(['development', 'test', 'production']),
   VITE_APP_VERSION: z.string(),
 });
 
 const env = envSchema.parse(import.meta.env);
+
+console.log(env);
 
 export default env;
