7cec17f8ac2ee2edaf18a5c4a6d370aef009abaa
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-04-24 20:19:34 -0400

    vite env cleanup

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string;
-  readonly VITE_APP_ENVIRONMENT: 'dev' | 'qa' | 'staging' | 'prod' | 'cypress';
-  readonly MODE: 'development' | 'test' | 'production';
-  readonly VITE_APP_VERSION: string;
-  readonly VITE_APP_MSW_ENABLED: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
