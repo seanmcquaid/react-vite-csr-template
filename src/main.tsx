04a9b9692622f11a954ec6ab2026c69e66fddaf6
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-05-11 22:24:54 -0600

    error boundary and auto merge

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { lazy, startTransition, StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { ErrorBoundary } from 'react-error-boundary';
 import env from './env';
 
 const App = lazy(() => import('./App'));
@@ -17,9 +18,11 @@ prepare().then(() =>
   startTransition(() => {
     createRoot(document.getElementById('root') as HTMLElement).render(
       <StrictMode>
-        <Suspense>
-          <App />
-        </Suspense>
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+          <Suspense>
+            <App />
+          </Suspense>
+        </ErrorBoundary>
       </StrictMode>,
     );
   }),
